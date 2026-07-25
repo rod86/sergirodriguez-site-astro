de0bc6a0ff35fc000e12dcf5022bce8546b16225
Sergi Rodriguez <sergi86@gmail.com>
2026-07-25 17:05:31 +0200

    update eslint deprecated

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,27 @@
+import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import astro from 'eslint-plugin-astro';
 import globals from 'globals';
 
-export default tseslint.config(
-  {
-    ignores: ['dist/', '.astro/', 'coverage/', 'node_modules/'],
-  },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...astro.configs['flat/recommended'],
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.browser,
-      },
+export default defineConfig(
+    {
+        ignores: ['dist/', '.astro/', 'coverage/', 'node_modules/'],
     },
-  },
-  {
-    rules: {
-      indent: ['error', 4],
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...astro.configs['flat/recommended'],
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+            },
+        },
     },
-  },
+    {
+        rules: {
+            indent: ['error', 4],
+        },
+    },
 );
