2ab07b4c596fa964baba9f2bb0d4dd4060dd9257
Sergi Rodriguez <sergi86@gmail.com>
2026-08-19 12:17:25 +0200

    contact form message

--- src/lib/contactForm/contactForm.ts
+++ src/lib/contactForm/contactForm.ts
@@ -1,28 +1,6 @@
-import type {SendContactMessageFunction} from "@lib/contactForm/types.ts";
+import type {ContactFormData} from "@lib/contactForm/types.ts";
+import {sendContactMessage} from "@lib/contactForm/web3forms.ts";
 
-
-export const submitContactForm: SendContactMessageFunction = async (data) => {
-    const formData = new FormData();
-    formData.append("name", data.name);
-    formData.append("email", data.email);
-    formData.append("message", data.message);
-    formData.append("access_key", import.meta.env.PUBLIC_WEB3FORMS_ACCESS_TOKEN);
-    const body = JSON.stringify(Object.fromEntries(formData));
-
-    const response = await fetch(
-        "https://api.web3forms.com/submit",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-            body
-        }
-    );
-    const json = await response.json();
-
-    if (!response.ok || !json.success) {
-        throw new Error("Failed to send a message: " + (json.message ?? json.error));
-    }
+export const submitContactFormData = async (data: ContactFormData): Promise<void> => {
+    return sendContactMessage(data);
 }
