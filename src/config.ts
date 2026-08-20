39166ca00364da50f7b86cb07780b54adde456c7
Sergi Rodriguez <sergi86@gmail.com>
2026-08-20 12:12:05 +0200

    centralized config file

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+
+export default {
+    siteDomain: "sergirodriguezwebsite.com",
+    title: "Full Stack Developer",
+    description: "Sergi Rodriguez — Full Stack Developer with 10+ years building scalable web applications with PHP, Node.js, TypeScript, Python and React.",
+    linkedin: "https://www.linkedin.com/in/sergirodriguez86/",
+    github: "https://github.com/rod86",
+
+    web3Forms: {
+        accessToken:  import.meta.env.PUBLIC_WEB3FORMS_ACCESS_TOKEN,
+    }
+} as const;
--- src/lib/contactForm/index.ts
+++ src/lib/contactForm/index.ts
@@ -1,8 +1,7 @@
 import {Web3FormsContactMessage} from "@lib/contactForm/contactMessage/Web3FormsContactMessage.ts";
 import {SendContactMessageHandler} from "@lib/contactForm/SendContactMessageHandler.ts";
+import config from "@config";
 
-const web3formsContactMessage = new Web3FormsContactMessage(
-    import.meta.env.PUBLIC_WEB3FORMS_ACCESS_TOKEN
-);
+const web3formsContactMessage = new Web3FormsContactMessage(config.web3Forms.accessToken);
 
 export const sendContactFormMessageHandler = new SendContactMessageHandler(web3formsContactMessage);
